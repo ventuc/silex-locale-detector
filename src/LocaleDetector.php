7dc4d4ca13328de66340d5493aa8d9dd09a12aef
Claudio Venturini <claudio.venturini1@tiscali.it>
2016-09-21 00:19:48 +0200

    Matching of preferred locales with a set of available languages

--- src/LocaleDetector.php
+++ src/LocaleDetector.php
@@ -17,7 +17,7 @@ class LocaleDetector {
 	public function __construct(Request $request){
 		$preferredLocales = array();
 		foreach ($request->getLanguages() as $locale){
-			$preferredLocales[] = new Locale($locale);
+			$preferredLocales[] = Locale::fromLocaleString($locale);
 		}
 		
 		$this->preferredLocales = $preferredLocales;
@@ -26,13 +26,13 @@ class LocaleDetector {
 	/**
 	 * Get the locale preferred by the user
 	 * 
-	 * @return LocalePreference The highest priority
+	 * @return Locale The highest priority
 	 * preference, or null if the user has no preference
 	 */
-	public function getUserPreferredLocale() {
+	public function getPreferredLocale() {
 		$preferred = null;
 		
-		$preferences = $this->getUserPreferredLocales();
+		$preferences = $this->getPreferredLocales();
 		if (count($preferences)){
 			$preferred = $preferences[0];
 		}
@@ -47,8 +47,64 @@ class LocaleDetector {
 	 * (from preferences with higher priority to
 	 * those with lowest priority)
 	 */
-	public function getUserPreferredLocales(){
+	public function getPreferredLocales(){
 		return $this->preferredLocales;
+	}
+	
+	/**
+	 * Get the locale preferred by the user among those available,
+	 * or the given default locale if none of the user's preferred
+	 * locales is available.
+	 * 
+	 * @param Locale[] $availableLocales The locales available
+	 * @param Locale $defaultLocale The default locale
+	 * @return Locale The preferred available locale, or the default locale
+	 */
+	public function getPreferredAvailableLocale(array $availableLocales, Locale $defaultLocale = null) {
+		$preferred = $defaultLocale;
+		
+		$preferences = $this->getPreferredAvailableLocales($availableLocales);
+		if (count($preferences)){
+			$preferred = $preferences[0];
+		}
+		
+		return $preferred;
+	}
+	
+	/**
+	 * Get the user's preferred locale among those available
+	 * 
+	 * @param Locale[] $availableLocales The locales available
+	 * against which to match the user's preferences
+	 * @return array Preferences sorted by priority
+	 * (from preferences with higher priority to
+	 * those with lowest priority)
+	 */
+	public function getPreferredAvailableLocales(array $availableLocales = null){
+		// First search a locale matching exactly (both language and country)
+		$preferredAvailableLocales = array();
+		foreach ($this->preferredLocales as $locale){
+			foreach ($availableLocales as $availableLocale){
+				if ($locale->equals($availableLocale)){
+					$preferredAvailableLocales[] = $locale;
+					break;
+				}
+			}
+		}
+		
+		// Else search a locale matching only by language
+		if (!$preferredAvailableLocales){
+			foreach ($this->preferredLocales as $locale){
+				foreach ($availableLocales as $availableLocale){
+					if ($locale->equalsLanguage($availableLocale)){
+						$preferredAvailableLocales[] = $locale;
+						break;
+					}
+				}
+			}
+		}
+		
+		return $preferredAvailableLocales;
 	}
 	
 }
